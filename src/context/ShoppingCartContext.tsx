417c5616710ba0a5358e6feb7aa8a14b3a4ba7f0
Karan Chavan <103585419+Amaterasu-08@users.noreply.github.com>
2023-10-24 22:55:19 +0530

    Add files via upload

---
+++ src/context/ShoppingCartContext.tsx
@@ -0,0 +1,109 @@
+import { ReactNode, createContext, useContext, useState } from "react";
+import { ShoppingCart } from "../components/ShoppingCart";
+import { useLocalStorage } from "../hooks/useLocalStroage";
+
+type ShoppingCartContext = {
+  getItemQuantity: (id: number) => number;
+  increaseCartQuantity: (id: number) => void;
+  decreaseCartQuantity: (id: number) => void;
+  removeFromCart: (id: number) => void;
+  openCart: () => void;
+  closeCart: () => void;
+  cartQuantity: number;
+  cartItems: cartItem[];
+};
+
+// only two elements required if we have id we can find the item if we have the quantity we can find the price
+interface cartItem {
+  id: number;
+  quantity: number;
+}
+
+//context lets the parent component make some info available to any component in the tree below so no need to specify it through props everytime
+const ShoppingCartContext = createContext({} as ShoppingCartContext);
+
+export function useShoppingCart() {
+  return useContext(ShoppingCartContext);
+}
+
+type ShoppingCartProviderProps = {
+  children: ReactNode;
+};
+export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
+  // earlier const [cartItem, setCartItems] = useState<CartItem[]>([]) then it got changes to the below one
+  const [cartItems, setCartItems] = useLocalStorage<cartItem[]>(
+    "shopping-cart",
+    []
+  );
+  const [isOpen, setIsOpen] = useState(false);
+
+  const cartQuantity = cartItems.reduce(
+    (quantity, item) => item.quantity + quantity,
+    0
+  );
+
+  const openCart = () => setIsOpen(true);
+  const closeCart = () => setIsOpen(false);
+
+  function getItemQuantity(id: number) {
+    return cartItems.find((item) => item.id == id)?.quantity || 0;
+  }
+
+  function increaseCartQuantity(id: number) {
+    //currItem is the current list of item we are operating on
+    setCartItems((currItems) => {
+      // if the current item is not there then add it else in increase it
+      if (currItems.find((item) => item.id === id) == null) {
+        return [...currItems, { id, quantity: 1 }];
+      } else {
+        return currItems.map((item) => {
+          if (item.id === id) {
+            return { ...item, quantity: item.quantity + 1 };
+          } else {
+            return item;
+          }
+        });
+      }
+    });
+  }
+
+  function decreaseCartQuantity(id: number) {
+    setCartItems((currItem) => {
+      if (currItem.find((item) => item.id === id)?.quantity === 1) {
+        return currItem.filter((item) => item.id !== id);
+      } else {
+        return currItem.map((item) => {
+          if (item.id === id) {
+            return { ...item, quantity: item.quantity - 1 };
+          } else {
+            return item;
+          }
+        });
+      }
+    });
+  }
+
+  function removeFromCart(id: number) {
+    setCartItems((currItem) => {
+      return currItem.filter((item) => item.id !== id);
+    });
+  }
+
+  return (
+    <ShoppingCartContext.Provider
+      value={{
+        getItemQuantity,
+        increaseCartQuantity,
+        decreaseCartQuantity,
+        removeFromCart,
+        cartItems,
+        cartQuantity,
+        openCart,
+        closeCart,
+      }}
+    >
+      {children}
+      <ShoppingCart isOpen={isOpen} />
+    </ShoppingCartContext.Provider>
+  );
+}
